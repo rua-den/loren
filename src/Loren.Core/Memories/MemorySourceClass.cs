2548727a4f936343bf15ab41f5ef6ecbfc79d8e4
turtle <nhkhuy241@gmail.com>
2026-09-04 16:50:16 +0700

    feat: add memory source authority classes

---
+++ src/Loren.Core/Memories/MemorySourceClass.cs
@@ -0,0 +1,11 @@
+namespace Loren.Core.Memories;
+
+public enum MemorySourceClass
+{
+    OwnerExplicit,
+    OwnerCorrection,
+    VerifiedTool,
+    OwnerApprovedInference,
+    ModelInference,
+    ExternalContent,
+}
