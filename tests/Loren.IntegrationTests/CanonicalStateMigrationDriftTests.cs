abd5ea046202772139582a5fade8f12192758e17
turtle <nhkhuy241@gmail.com>
2026-09-05 00:01:33 +0700

    fix: import migration operation diagnostics

--- tests/Loren.IntegrationTests/CanonicalStateMigrationDriftTests.cs
+++ tests/Loren.IntegrationTests/CanonicalStateMigrationDriftTests.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Xunit;
 
 namespace Loren.IntegrationTests;
